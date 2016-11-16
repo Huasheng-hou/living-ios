4b69c6ad10cb97e85513d5304d3c868e1b879c87
侯华生 <h@huancheclub.com>
2016-11-16 21:03:54 +0800

    付费成为加盟商页面，增加微信支付的失败回调

--- living/living/Controller/person/2DCode/LMFranchiseeViewController.m
+++ living/living/Controller/person/2DCode/LMFranchiseeViewController.m
@@ -59,6 +59,20 @@ liveNameProtocol
     [super viewDidLoad];
     [self createUI];
     
+    // * 微信支付被用户取消
+    //
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(wxPayCanceled)
+                                                 name:LM_WECHAT_PAY_CANCEL_NOTIFICATION
+                                               object:nil];
+    
+    // * 微信支付失败
+    //
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(wxPayFailed)
+                                                 name:LM_WECHAT_PAY_FAILED_NOTIFICATION
+                                               object:nil];
+    
     // * 微信支付结果确认
     //
     [[NSNotificationCenter defaultCenter] addObserver:self
@@ -272,7 +286,7 @@ liveNameProtocol
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (indexPath.section==0) {
-        NSLog(@"***");
+        
         LMChangeLivingController *livingVC = [[LMChangeLivingController alloc] init];
         livingVC.delegate=self;
         [self.navigationController pushViewController:livingVC animated:YES];
@@ -323,18 +337,26 @@ liveNameProtocol
 {
     if (![CheckUtils isLink]) {
         
-        [self textStateHUD:@"无网络连接"];
+        [self textStateHUD:@"无网络"];
         return;
     }
+    
     [self initStateHud];
-    LMFranchiseeWchatPayRequest *request=[[LMFranchiseeWchatPayRequest alloc] initWithWXRecharge:@"3600" andLivingUuid:_liveUUID andPhone:headcell.NumTF.text andName:headcell.NameTF.text];
+    
+    LMFranchiseeWchatPayRequest     *request    = [[LMFranchiseeWchatPayRequest alloc] initWithWXRecharge:@"3600"
+                                                                                            andLivingUuid:_liveUUID
+                                                                                                 andPhone:headcell.NumTF.text
+                                                                                                  andName:headcell.NameTF.text];
+    
     HTTPProxy   *proxy  = [HTTPProxy loadWithRequest:request
                                            completed:^(NSString *resp, NSStringEncoding encoding) {
                                                
                                                [self performSelectorOnMainThread:@selector(wxRechargeResponse:)
                                                                       withObject:resp
                                                                    waitUntilDone:YES];
+    
                                            } failed:^(NSError *error) {
+                                           
                                                [self textStateHUD:@"微信充值下单失败"];
                                            }];
     [proxy start];
