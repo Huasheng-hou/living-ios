0843470c97998e724fb6eff3f749b07cf6342dc0
侯华生 <h@huancheclub.com>
2017-01-21 18:07:46 +0800

    调整直播间代码结构，使其不再直接集成自基类

--- living/living/Controller/find/Lesson/LMChatViewController.h
+++ living/living/Controller/find/Lesson/LMChatViewController.h
@@ -14,13 +14,15 @@ typedef enum {
     kFitMessageTableStateIdle         =   2,
 } FitMessageTableState;
 
-@interface LMChatViewController : FitStatefulTableViewController <STOMPClientDelegate>
+@interface LMChatViewController : FitTableViewController <STOMPClientDelegate>
 
-@property (nonatomic,strong)NSString *voiceUuid;
-@property (nonatomic,strong)NSString *sign;
-@property (nonatomic,strong)NSString *roles;
+@property (nonatomic,strong)    NSString        *voiceUuid;
+@property (nonatomic,strong)    NSString        *sign;
+@property (nonatomic,strong)    NSString        *roles;
+@property (nonatomic,retain)    NSMutableArray  *listData;
 
 @property (assign, nonatomic)   NSInteger               total;
+@property (nonatomic)           int                     max;
 @property (assign, nonatomic)   FitMessageTableState    state;
 
 @end
