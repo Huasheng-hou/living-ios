c18115e6d293fe149a6972ece11b853bb3d64ccb
陈乐 <cl@huancheclub.com>
2017-01-24 09:58:47 +0800

    提交版本

--- living/living/Model/websocket/LMWobsocket.m
+++ living/living/Model/websocket/LMWobsocket.m
@@ -14,7 +14,8 @@ static STOMPClient *client;
 
 + (STOMPClient *)shareWebsocket
 {
-    NSURL *websocketUrl = [NSURL URLWithString:@"ws://121.43.40.58/live-connect/websocket"];
+    
+    NSURL *websocketUrl = [NSURL URLWithString:@"ws://websocket.yaoguo1818.com/live-connect/websocket"];
     
     if (!client) {
     
