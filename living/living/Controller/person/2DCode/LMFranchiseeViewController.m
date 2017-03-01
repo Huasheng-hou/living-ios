2c79f9a3f0596a4d604aa20631e57795a65cd068
陈乐 <cl@huancheclub.com>
2017-03-01 18:20:04 +0800

    加盟支付修改

--- living/living/Controller/person/2DCode/LMFranchiseeViewController.m
+++ living/living/Controller/person/2DCode/LMFranchiseeViewController.m
@@ -769,7 +769,7 @@ liveNameProtocol
         
         if ([[bodyDict objectForKey:@"result"] isEqualToString:@"0"]){
             
-            if ([_xufei isEqualToString:@"xufei"]) {
+            if (_franchisee &&[_franchisee isEqualToString:@"yes"]) {
                 [self textStateHUD:@"续费成功！"];
                 dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                     [[NSUserDefaults standardUserDefaults] setObject:@"2" forKey:@"xufei_dot"];
